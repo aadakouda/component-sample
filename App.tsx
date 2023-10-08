4c0321b5176ebca8a7c12d7d731c21ca4b137e54
aadakouda <kurottsu@gmail.com>
2023-10-09 00:04:47 +0900

    fix

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, StatusBar as SB, Button, Pressable } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, StatusBar as SB, Pressable } from 'react-native';
+import * as Haptics from 'expo-haptics'
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
+import AntDesignIcon from 'react-native-vector-icons/AntDesign'
 
 // background: linear-gradient(220.55deg, #FF5E98 0%, #0F213E 100%);
 // background: linear-gradient(220.55deg, #FF9D7E 0%, #4D6AD0 100%);
@@ -20,10 +23,23 @@ export default function App() {
 ['#000', '#000'],
   ]
   const [colors, setColors] = useState(gradientsSample[3])
+  const card = {
+    front: "おもて",
+    back: "うら"
+  }
+  const [isFront, setIsFront] = useState<boolean>(true)
+  const reverseCard = () => {
+    setIsFront(!isFront)
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+  }
+  const [isTapped, setIsTapped] = useState<boolean>(false)
   return (
-    <SafeAreaView style={{paddingTop: SB.currentHeight}}>
+    <SafeAreaView style={{paddingTop: SB.currentHeight, flex: 1}}>
       <LinearGradient colors={colors} start={{x: 0, y: 1}} end={{x: 1, y: 0}} style={styles.background} />
-      <ScrollView style={{width: 'auto', height: 'auto'}}>
+      <View style={{width: '100%', alignItems: 'center', padding: 10}}>
+        <Text style={{color: '#FFF', fontSize: 25}}>Header</Text>
+      </View>
+      <ScrollView>
         <View style={styles.container}>
           <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
           <Text> </Text>
@@ -40,6 +56,39 @@ export default function App() {
             ))
           }
           <Text> </Text>
+          <Pressable onPress={reverseCard} style={styles.sample2}>
+            <View style={{alignItems: 'center'}}>
+              <Text style={[styles.cardText, {...!isFront && {color: 'rgba(256, 256, 256, 0.7)', fontSize: 23}}]}>{card.front}</Text>
+              {!isFront && 
+                <Text style={[styles.cardText, {paddingTop: 7}]}>{card.back}</Text>
+              }
+            </View>
+          </Pressable>
+          <Text> </Text>
+          <Pressable onPress={() => setIsTapped(!isTapped)} style={styles.sample2}>
+            <View style={styles.separetedCard}>
+              <Text style={styles.cardText}>{card.front}</Text>
+            </View>
+            <View style={{height: 0.5, width: '100%', backgroundColor: 'rgba(256, 256, 256, 0.5)'}} />
+            <View style={styles.separetedCard}>
+              {!isTapped ?
+                (<FontAwesome5Icon name='hand-pointer' size={20} style={styles.cardText} />) : (<Text style={styles.cardText}>{card.back}</Text>)
+              }
+            </View>
+          </Pressable>
+          <Text> </Text>
+          <Pressable onPress={() => setIsTapped(!isTapped)} style={styles.sample2}>
+            <View style={styles.separetedCard}>
+              <Text style={styles.cardText}>{card.front}</Text>
+            </View>
+            <View style={{height: 0.5, width: '100%', backgroundColor: 'rgba(256, 256, 256, 0.5)'}} />
+            <View style={styles.separetedCard}>
+              {!isTapped ?
+                (<AntDesignIcon name='question' size={20} style={styles.cardText} />) : (<Text style={styles.cardText}>{card.back}</Text>)
+              }
+            </View>
+          </Pressable>
+          <Text> </Text>
           <View style={styles.sample2}>
             <Text style={styles.text}>hello world</Text>
             <Text style={styles.text}>イメージはこんな感じ テステス</Text>
@@ -55,9 +104,13 @@ export default function App() {
             <Text style={styles.text}>イメージはこんな感じ テステス</Text>
             <Text style={styles.text}>あああああああああああああああああああああああああああああああああああああああああああああ</Text>
           </View>
-          <Text> </Text>
         </View>
       </ScrollView>
+      <View style={{width: '100%', paddingTop: 15, paddingBottom: 15, display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+        <FontAwesome5Icon name="tasks" size={25} style={{color: '#FFF'}} />
+        <FontAwesome5Icon name="edit" size={25} style={{color: '#FFF'}} />
+        <FontAwesome5Icon name="wrench" size={25} style={{color: '#FFF'}} />
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -73,22 +126,28 @@ const styles = StyleSheet.create({
   text: {
     color: '#FFF',
   },
-  sample: {
-    borderRadius: 7,
-    padding: 20,
+  cardText: {
+    color: '#FFF',
+    fontSize: 30,
+  },
+  separetedCard: {
+    flex: 1,
     alignItems: 'center',
-    borderColor: 'rgba(256, 256, 256, 0.2)',
-    borderStyle: 'solid',
-    borderWidth: 0.5,
+    justifyContent: 'center',
+    minHeight: 150,
   },
   sample2: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 7,
     padding: 20,
-    alignItems: 'center',
     backgroundColor: 'rgba(256, 256, 256, 0.1)',
-    borderColor: 'rgba(256, 256, 256, 0.15)',
+    borderColor: 'rgba(256, 256, 256, 0.5)',
     borderStyle: 'solid',
     borderWidth: 1,
+    width: '100%',
+    minHeight: 300,
   },
   sample3: {
     borderRadius: 7,
